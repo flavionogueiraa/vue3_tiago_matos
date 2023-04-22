e7199010b8248801a59aa1e779c44f7effaab54d
flavio.nogueiraa <flavio.nogueira.profissional@gmail.com>
2023-04-21 22:31:53 -0300

    First commit

---
+++ app/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <TheHeader />
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    TheHeader,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
