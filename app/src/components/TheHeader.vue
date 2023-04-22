94aaf0a5a122a8778f06ba0974077bd6fd95bfa9
flavio.nogueiraa <flavio.nogueira.profissional@gmail.com>
2023-04-22 07:08:12 -0300

    Lesson5: v-for

--- app/src/components/TheHeader.vue
+++ app/src/components/TheHeader.vue
@@ -1,18 +1,60 @@
 <template>
-    <header class="header">
-        Header
-    </header>
+  <div>
+    <div class="todos-items" v-for="(obj, index) in todos" v-bind:key="obj.id">
+      {{ index }} - {{ obj.title }}
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-
-    }
+export default {
+  name: "App",
+  data() {
+    return {
+      todos: [
+        {
+          userId: 1,
+          id: 1,
+          title: "delectus aut autem",
+          completed: false,
+        },
+        {
+          userId: 1,
+          id: 2,
+          title: "quis ut nam facilis et officia qui",
+          completed: false,
+        },
+        {
+          userId: 1,
+          id: 3,
+          title: "fugiat veniam minus",
+          completed: false,
+        },
+        {
+          userId: 1,
+          id: 4,
+          title: "et porro tempora",
+          completed: true,
+        },
+        {
+          userId: 1,
+          id: 5,
+          title:
+            "laboriosam mollitia et enim quasi adipisci quia provident illum",
+          completed: false,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-    .header {
-        background: black;
-        color: white;
-    }
+.todos-items {
+    background: black;
+    margin: 0 0 5px 0;
+    padding: 3px 6px;
+    color: white;
+    border-radius: 5px;
+}
 </style>
