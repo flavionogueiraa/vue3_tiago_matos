9849b324d43936ccb49a225be3350a0256ee47a1
flavio.nogueiraa <flavio.nogueira.profissional@gmail.com>
2023-05-20 08:30:14 -0300

    Lesson25 - Introdução a composition API Reatividade

--- fourth_app/src/App.vue
+++ fourth_app/src/App.vue
@@ -1,7 +1,22 @@
 <template>
   <div>
-    {{ name }}
-    <AppProduct></AppProduct>
+    {{ name }} <br /><br />
+    <h5>User</h5>
+    {{ user.first_name }}
+    {{ user.last_name }}
+    <br />
+    <h5>Admin</h5>
+    {{ admin.first_name }}
+    {{ admin.last_name }}
+    <br />
+
+    {{ count }}
+
+    <button @click="count++">+</button>
+    <button @click="count--">-</button>
+    <br />
+
+    <!-- <AppProduct></AppProduct> -->
 
     <img @click="changeName()" alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
@@ -9,24 +24,43 @@
 </template>
 
 <script>
+import { reactive, ref } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
-import AppProduct from "./components/Products/AppProduct.vue";
+// import AppProduct from "./components/Products/AppProduct.vue";
 
 export default {
   name: "App",
   components: {
-    AppProduct,
+    // AppProduct,
     HelloWorld,
   },
   setup() {
+    const user = reactive({
+      first_name: "John",
+      last_name: "Wick",
+    });
+
+    const admin = ref({
+      first_name: "Admin",
+      last_name: "Master",
+    });
+
+    const count = ref(0);
+    console.log(count);
+
     let name = "Flávio";
     const changeName = () => {
-      alert("changeName");
+      // alert("changeName");
       name = "John Wick";
+      user.first_name = "Example";
+      admin.value.first_name = "Other example";
     };
 
     return {
+      admin,
+      user,
       name,
+      count,
       changeName,
     };
   },
